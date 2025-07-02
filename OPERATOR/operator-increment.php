0158f68759c888e2288201d2ba4b5cfaf3c62209
bunga <bungananta2022@gmail.com>
2025-07-02 11:50:29 +0700

    feat:Upload folder berisi latihan aritmatika, logika, perbandingan, Penugasan, Increment

---
+++ OPERATOR/operator-increment.php
@@ -0,0 +1,15 @@
+<?php
+echo "<h3>Latihan Increment</h3>";
+
+$x = 15;
+
+echo "Nilai awal: $x <br>";
+
+//Post-increment 
+echo "Post-increment (x++): " . $x++ . "<br>";
+echo "Setelah post-increment, x = $x <br><br>";
+
+//Pre-increment
+echo "pre-increment (++x): " . ++$x . "<br>";
+echo "Setelah pre-increment, x = $x <br>";
+?>
---
+++ OPERATOR/operator-penugasan.php
@@ -0,0 +1,24 @@
+<?php
+$x = 80;
+echo "Nilai awal: $x <br>";
+
+//tambah
+$x += 10;
+echo "Setelah += 10: $x <br>";
+
+//kurang
+$x -= 2;
+echo "Setelah -= 2: $x <br>";
+
+//kali
+$x *= 7;
+echo "Setelah *= 7: $x <br>";
+
+//bagi
+$x /= 4;
+echo "Setelah /= 4: $x <br>";
+
+//modulus
+$x %= 3;
+echo "Setelah %= 3: $x <br>";
+?>
