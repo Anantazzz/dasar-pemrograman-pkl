eceb887bcec9e0a648f88fa6059c6482ad0653bb
bunga <bungananta2022@gmail.com>
2025-07-02 14:07:22 +0700

    feat:Upload folder berisi latihan perbandingan, perulangan, array, fungsi

---
+++ CAMPURAN/array.php
@@ -0,0 +1,17 @@
+<?php
+
+//Array
+$buah = ["Anggur", "Stroberi", "Manggis", "Melon"];
+echo "Buah Kedua: " . $buah[1] . "<br>";
+
+//Array associative
+$profile = [
+    "nama" => "Ananta",
+    "umur" => 17,
+    "hobi" => "Membaca"
+];
+
+    echo "Nama saya: " . $profile["nama"] . "<br>";
+    echo "Umur saya: " . $profile["umur"] . "<br>";
+    echo "Hobi saya: " . $profile["hobi"] . "<br>";
+?>
